aa91584f0b1819268eab2ddd9c1cc37e865fa52c
ashwinighodake <ashwini752000@gmail.com>
2021-11-16 13:48:30 +0530

    update linked_list_collection.java

--- Linked_list_collection.java
+++ Linked_list_collection.java
@@ -22,6 +22,28 @@ public class Linked_list_collection
           ch=sobj.next().charAt(0);
         }
         System.out.println("Contents of linked list are:"+l1);
+
+        //Insert data at specified position
+        l1.add(3,"Prajakta");
+        System.out.println("Updated Linked list:"+l1);
+
+        //remove first element from linked list
+        l1.removeFirst();
+        System.out.println("First Element removed: "+l1);
+
+        //replacing the specified position element
+        l1.set(3,"pranali");
+        System.out.println("Upadated linked list:"+ l1);
+        
+        System.out.println("Number of elements in the linked list are:"+l1.size());
+
+        
+        //by using iterator to iterate the linked list
+        Iterator it=l1.iterator();
+        while(it.hasNext())
+        {
+            System.out.println(it.next()+" ");
+        }
     }
     
 }
